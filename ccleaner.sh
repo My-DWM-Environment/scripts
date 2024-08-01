3d25bb897a2ac96dbc723e640683c065cf7d63ae
Chugunov Roman <Zebs-BMK@yandex.ru>
2024-08-01 15:42:29 +0300

    feat: add the ccleaner script

---
+++ ccleaner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -r -d '' LIST << EOM
+/var/lib/docker/overlay2/*
+/var/lib/docker/image/*
+/var/lib/docker/containers/*
+/var/lib/docker/volumes/*
+/var/log/journal/*
+/var/cache/pacman/pkg/*
+/home/ruut/.cache/yay/*
+/home/ruut/.cache/yarn/*
+/home/ruut/.cache/electron/*
+EOM
+
+while IFS= read -r line; do
+  echo "rm: $line";
+  sudo bash -c "rm -rf $line &";
+done <<< "$LIST"
